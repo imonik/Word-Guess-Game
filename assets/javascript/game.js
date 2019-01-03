28b3e0917567edefd10f9e5ae48bc7ffe88d1783
imonik <mk.santb@gmail.com>
2019-01-03 13:37:49 -0800

    added images and change layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,147 @@
-var words = ["awkward a", "banjo a", "cat a", "machine a", "cake a", "land a", "camp a", "air a", "battle a", "starbucks a" ];
-var currentWord = "";
-var maxAttempts = 0;
-var wonGames = 0;
-var foundLettersIndex = [];
+var _words = ["cake", "croissant", "chocolate", "cinnamon bun", "danish pastry", "soup", "camp a", "mocha", "pie", "coffee" ];
+var _wordList = [
+{special: "lobster", description : "Lorem Ipsum", image : "lobster.png"},
+{special: "noodles", description : "Lorem Ipsum", image : "noodles.png"},
+{special: "pancakes", description : "Lorem Ipsum", image : "pancakes.png"},
+{special: "pizza", description : "Lorem Ipsum", image : "pizza.png"},
+{special: "ramen", description : "Lorem Ipsum", image : "ramen.png"},
+{special: "rice", description : "Lorem Ipsum", image : "rice.png"},
+{special: "salad", description : "Lorem Ipsum", image : "salad.png"},
+{special: "onion soup", description : "Lorem Ipsum", image : "soup.png"},
+{special: "spaguetti", description : "Lorem Ipsum", image : "spaguetti.png"},
+{special: "tacos", description : "Lorem Ipsum", image : "tacos.png"}, 
+{special: "buttered shrimp", description : "Lorem Ipsum", image : "shrimp.png"}
+  ];
+var _imagePrefix = "./assets/images/";
+var _currentWord = "";
+var _remainingAttemps = 0;
+var _wonGames = 0;
+var _foundLettersIndex = [];
 var usedLetters = [];
 var maskedString = "";
 var _gameInProgress = false;
+var _currentPosition = "";
+
 
 function startGame() {
+	console.log("startGame");
 	var randomIndex = Math.floor((Math.random() * 10));
-	currentWord = words[randomIndex];
-	maxAttempts = currentWord.length-1 + 4;
+	_currentPosition = randomIndex;
+	_currentWord = _wordList[randomIndex].special;
+	_remainingAttemps = _currentWord.length-1 + 4;
 
-	for (var i = 0; i < currentWord.length; i++) {
-		currentWord[i] == " " ? maskedString += "&nbsp " : maskedString += "_ ";
+	for (var i = 0; i < _currentWord.length; i++) {
+		_currentWord[i] == " " ? maskedString += "&nbsp " : maskedString += "_ ";
 	}
 
 	document.getElementById("hiddenWord").innerHTML = maskedString;
-	document.getElementById("remainingAttemps").innerHTML = `Remaining attemps: ${maxAttempts}`;
+	diplayCoffeeAttemps();
 	document.getElementById("message").innerHTML ="";
-
+ 
 	_gameInProgress = true;
 }
 
+function diplayCoffeeAttemps(){
+	var attempImages ="";
+	var missedAttemps = usedLetters.length;
+
+	for(var i = 1; i <= usedLetters.length; i++) {
+		attempImages += `<img id="coffee${i}" src="./assets/images/bad-coffee.jpg">`
+	}
+
+	for(var i = 1; i <= _remainingAttemps; i++) {
+		attempImages += `<img id="coffee${i}" src="./assets/images/coffee1.png">`
+
+	}
+	document.getElementById("imageAttemps").innerHTML = attempImages;
+}
+
+function showWord(){
+	document.getElementById("hiddenWord").innerHTML = _currentWord;
+	document.getElementById("dish").src= `${_imagePrefix}${_wordList[_currentPosition].image}`;
+
+}
+ 
 window.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
     case 13: // start
     	if (!_gameInProgress) {
+     		clearAll();
       		startGame();
     	}
     	break;
     default:
-		checkForLetter(event.key,event.keyCode);
+    	if (_gameInProgress) {
+      		checkForLetter(event.key,event.keyCode);
+    	}
     	break;
   }
 }, false);
 
+function validateAttemps() {
+	if (_remainingAttemps == 0) {
+		document.getElementById("message").innerHTML ="You have reached all your attemps";
+		showWord();
+		_gameInProgress = false;
+		return;
+	}
+}
+
 function checkForLetter(letter, code) {
-	//if (!(code >= 97 && code <= 122 ) && !(code >= 65 && code <= 90) ) { // [a-z] or [A-Z]
 	if (!(code >= 65 && code <= 90)) { // A to Z non-case sensitive
 		return;
 	}
 
-	if (maxAttempts == 0) {
-		document.getElementById("message").innerHTML ="You have reached all your attemps";
-		_gameInProgress = false;
-		return;
-	}
+	validateAttemps();
 
-	if (currentWord.includes(letter)) {
+	if (_currentWord.includes(letter)) {
 		//usedLetters.push(letter);
 		getAllOcurrencesOfLetter(letter);
 		replaceFoundLetters();
 	} else {
 		hasBeenUsed(letter);
-		document.getElementById("usedLetters").innerHTML = usedLetters;
+		//document.getElementById("usedLetters").innerHTML = usedLetters;
+		displayUsedLetters();
 	}
+}
+
+function displayUsedLetters(){
+	//<div class="letterCircle">S</div>
+	var displayLetter = "";
+	for(var i = 0; i < usedLetters.length; i++) {
+		displayLetter += `<div class="letterCircle">${usedLetters[i]}</div>`
+
+	}
+	document.getElementById("styled-letter").innerHTML = displayLetter;
 }
 
 function hasBeenUsed(letter){
 	if (!usedLetters.includes(letter)) {
+		//to do verificar el numero de intentos y cambiar _in progress variable
 		usedLetters.push(letter);
-		maxAttempts--;
-		document.getElementById("remainingAttemps").innerHTML = `Remaining attemps: ${maxAttempts}`;
+		_remainingAttemps--;
+		//document.getElementById("remainingAttemps").innerHTML = `Remaining attemps: ${_remainingAttemps}`;
+		displayUsedLetters();
+		diplayCoffeeAttemps();
+		validateAttemps();
 	}
 }
 
 function getAllOcurrencesOfLetter(letter) {
 	//console.log(`Letter to find is: ${a}`);   
-	for (var i = 0; i < currentWord.length; i++) {
-		if (currentWord[i] == letter){
-			foundLettersIndex.push(i);
-			if (currentWord.replace(" ", "").length == foundLettersIndex.length) {
-				wonGames++;
-				document.getElementById("imgWins").src= `./assets/images/muffin${wonGames}.png`;
-				document.getElementById("wins").innerHTML = `Wins: ${wonGames}`;
-				document.getElementById("message").innerHTML ="YOU WON!";
-				clearAll();
-				window.setTimeout(startGame, 5000);
+	for (var i = 0; i < _currentWord.length; i++) {
+		if (_currentWord[i] == letter){
+			_foundLettersIndex.push(i);
+			if (_currentWord.replace(" ", "").length == _foundLettersIndex.length) {
+				_wonGames++;
+				document.getElementById("imgWins").src= `./assets/images/muffin${_wonGames+2}.png`;
+				//document.getElementById("wins").innerHTML = `Wins: ${_wonGames}`;
+				document.getElementById("message").innerHTML ="Congratulations! Here is you muffin. Try again to make it bigger!";
+				//agregar una animacion de ganar!
+				document.getElementById("dish").src= `${_imagePrefix}${_wordList[_currentPosition].image}`;
+				_gameInProgress = false;
+				//clearAll();
+				//window.setTimeout(startGame, 5000);
 			}
 		}
 	}
@@ -87,26 +151,30 @@ function replaceFoundLetters() {
 	var replacedWord = maskedString.trim().split(" ");
 
 	for (var i = 0; i < replacedWord.length; i++) {
-		for (var j = 0; j < foundLettersIndex.length; j++) {
-			if (i == foundLettersIndex[j]) {
-				replacedWord[i] = currentWord[foundLettersIndex[j]];
+		for (var j = 0; j < _foundLettersIndex.length; j++) {
+			if (i == _foundLettersIndex[j]) {
+				replacedWord[i] = _currentWord[_foundLettersIndex[j]];
 				maskedString = replacedWord.join(" ");
 			}
 		}
 	}
 
 	document.getElementById("hiddenWord").innerHTML = maskedString;
-	document.getElementById("usedLetters").innerHTML = usedLetters;
+	//document.getElementById("usedLetters").innerHTML = usedLetters;
+	displayUsedLetters();
 }
 
 function clearAll() {
-	currentWord = "";
-	maxAttempts = 0;
-	foundLettersIndex = [];
+	_currentWord = "";
+	_remainingAttemps = 0;
+	_foundLettersIndex = [];
 	usedLetters = [];
 	maskedString = "";
+
+	
 	document.getElementById("hiddenWord").innerHTML = maskedString;
-	document.getElementById("usedLetters").innerHTML = usedLetters;
-	document.getElementById("remainingAttemps").innerHTML = `Remaining attemps: ${maxAttempts}`;
+	//document.getElementById("usedLetters").innerHTML = usedLetters;
+	document.getElementById("styled-letter").innerHTML = usedLetters;
+	//document.getElementById("remainingAttemps").innerHTML = `Remaining attemps: ${_remainingAttemps}`;
 	document.getElementById("message").innerHTML ="";
 }
